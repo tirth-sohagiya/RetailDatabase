a38c492d11a74483adbeca84db8043697701188b
BigPetey72 <lppeters@ucdavis.edu>
2024-11-05 13:35:50 -0800

    Springboot Maven Initialize
    Created a new Maven project with Springboot. Added dependencies for connection to mysql

---
+++ onlineretail/src/main/java/com/csen280/onlineretail/OnlineretailApplication.java
@@ -0,0 +1,13 @@
+package com.csen280.onlineretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineretailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineretailApplication.class, args);
+	}
+
+}
---
+++ onlineretail/src/test/java/com/csen280/onlineretail/OnlineretailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csen280.onlineretail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineretailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
