60eaad72f79e4efbd6b5dbc77023b905941e338b
BigPetey72 <lppeters@ucdavis.edu>
2024-11-18 18:05:34 -0800

    Added my test db dump file

--- FlaskStore/test.py
+++ FlaskStore/test.py
@@ -15,19 +15,24 @@ for row in result.fetch_all():
 print(result.fetch_all())"""
 import pymysql.cursors
 
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='root',
-                             database='test',
+connection = pymysql.connect(host='aws.cjwc228ggyr7.us-west-1.rds.amazonaws.com',
+                             user='admin',
+                             password='password',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
 with connection:
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM User"
+        sql = "CREATE DATABASE IF NOT EXISTS test"
+        sql = "CREATE TABLE user (uid INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(40) NOT NULL, name VARCHAR(40) NOT NULL, pass VARCHAR(15) NOT NULL)"  
+        cursor.execute(sql)
+        #result = cursor.fetchall()
+        #print(result)
+        sql = "SELECT * FROM user"
         cursor.execute(sql)
         result = cursor.fetchall()
         print(result)
+        
 connection.ping()
 with connection:
     with connection.cursor() as cursor:
